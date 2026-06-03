7eeafaaea72c50f55339514bb4dda8c2b88f81bc
unknown <webhostingportfolio124@gmail.com>
2026-06-03 17:15:19 +0530

    fixed url error

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,23 @@
 
+// import axios from 'axios';
+
+// const API = axios.create({
+//   baseURL: 'https://quleep-3d-portal.onrender.com/api'
+// });
+
+// export const getProducts = (search = '') => API.get(`?search=${search}`);
+// export const getProductById = (id) => API.get(`/${id}`);
+// export const createProduct = (productData) => API.post('/', productData);
+
+
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://quleep-3d-portal.onrender.com/api'
+  // Base URL ko hum /api/products tak lekar jayenge
+  baseURL: 'https://quleep-3d-portal.onrender.com/api/products'
 });
 
+// Ab endpoints direct base URL ke aage perfectly map ho jayenge
 export const getProducts = (search = '') => API.get(`?search=${search}`);
 export const getProductById = (id) => API.get(`/${id}`);
 export const createProduct = (productData) => API.post('/', productData);
